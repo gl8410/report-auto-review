97d223fe44bf2d087de2a3388c8c3c2127842cd1
antrix <guilin8410@gmail.com>
2026-01-22 17:27:59 +0800

    update gitignore

--- components/RuleManager.tsx
+++ components/RuleManager.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { useResizable } from '../hooks/useResizable';
 import { Rule, RuleGroup } from '../types';
